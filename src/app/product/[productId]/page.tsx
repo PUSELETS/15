417b627b44339b0d6034dfe94ae3c59dd1f7e0e8
PUSELETS <dimamabolo15@gmail.com>
2024-11-19 23:37:59 -0800

    cart

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -9,6 +9,7 @@ import Link from 'next/link'
 import { notFound } from 'next/navigation'
 import { db } from '@/app/database'
 import { Query } from 'appwrite';
+import AddToCartButton from '@/components/AddToCartButton'
 
 
 const BREADCRUMBS = [
@@ -118,7 +119,7 @@ const filtered = Object.values(data)[1]
           <div className='mt-10 lg:col-start-1 lg:row-start-2 lg:max-w-lg lg:self-start'>
             <div>
               <div className='mt-10'>
-                
+                <AddToCartButton product={product} />
               </div>
               <div className='mt-6 text-center'>
                 <div className='group inline-flex text-sm text-medium'>
