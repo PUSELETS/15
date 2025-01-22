35b56987c4b525177407c2e802703a2678d94089
PUSELETS <dimamabolo15@gmail.com>
2025-01-21 20:23:12 -0800

    rose

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ const perks = [
 export default function Home() {
   return (
     <>
-      <MaxWidthWrapper className="bg-[#fffbeb]">
+      <MaxWidthWrapper className="bg-rose-200">
         <div className='py-20 mx-auto text-center flex flex-col items-center max-w-3xl '>
           <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
             Your marketplace for high-quality{' '}
