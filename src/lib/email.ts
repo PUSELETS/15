0fe301de39cd9e20feb7c4195777b755b3f4cb08
PUSELETS <dimamabolo15@gmail.com>
2025-10-04 02:47:23 -0700

    emi

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -18,9 +18,14 @@ export const setPayload = async (newPayload: string) => {
 
     const transporter = nodemailer.createTransport({
         service: 'gmail',
+        port: 587,
+        secure: false,
         auth: {
             user: process.env.GMAIL_USER,
             pass: process.env.GMAIL_PASS,
+        },
+        tls: {
+            rejectUnauthorized: false,
         },
     });
 
