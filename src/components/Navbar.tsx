6683310d7ec54e7bae5f3fd4fe888bec610ef996
PUSELETS <dimamabolo15@gmail.com>
2025-12-09 01:07:21 -0800

    dynamic

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -24,54 +24,16 @@ const Navbar = async () => {
 
               <div className=' flex items-center'>
                 <div className='flex flex-1 items-center justify-end space-x-6'>
-                  {user ? null : (
-                    <Link
-                      href='/sign-in'
-                      className={buttonVariants({
-                        variant: 'ghost',
-                      })}>
-                      Sign in
-                    </Link>
-                  )}
-
-                  {user ? null : (
-                    <span
-                      className='h-6 w-px bg-gray-200'
-                      aria-hidden='true'
-                    />
-                  )}
+                  
 
                   {user ? (
                     <UserAccountNav user={user} />
-                  ) : (
-                    <Link
-                      href='/sign-up'
-                      className={buttonVariants({
-                        variant: 'ghost',
-                      })}>
-                      Create account
-                    </Link>
-                  )}
-
-                  {user ? (
-                    <span
-                      className='h-6 w-px bg-gray-200'
-                      aria-hidden='true'
-                    />
-                  ) : null}
-
-                  {user ? null : (
-                    <div className='flex lg:ml-6'>
-                      <span
-                        className='h-6 w-px bg-gray-200'
-                        aria-hidden='true'
-                      />
-                    </div>
-                  )}
+                  ) : ''}
+                  
                 </div>
                 <div className='ml-4 flow-root lg:ml-6'>
                    
-                   <Cartt />
+                   <Cart/>
                 </div>
                 
               </div>
